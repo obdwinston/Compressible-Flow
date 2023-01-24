da98e6429b31652a5e832dd16264175f66f1eefa
Winston Ong <104728656+obdwinston@users.noreply.github.com>
2023-01-25 06:25:06 +0800

    Update movie.py

--- movie.py
+++ movie.py
@@ -40,5 +40,5 @@ for nt in range(Nt):
 image = []
 for nt in range(Nt):
     image.append(os.path.join(figure, 'U{:d}.png'.format(nt)))
-clip = mpy.ImageSequenceClip(image, fps=int(Nt/20))
+clip = mpy.ImageSequenceClip(image, fps=int(Nt/10))
 clip.write_videofile(movie)
